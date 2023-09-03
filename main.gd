b5641a510a3e4e130450b031a0eeb2c98b06c8ba
Eibriel <eibriel@eibriel.com>
2023-09-03 11:27:56 -0300

    cursor and pointer as separated things

--- main.gd
+++ main.gd
@@ -9,7 +9,8 @@ var nodes: Array[Area2D]
 #@onready var cursor3D = $Game/CSGSphere3D
 @onready var buttons := $UI/MarginContainer/Buttons
 #@onready var compicactus_anim = $Game/Compicactus/AnimationPlayer
-@onready var cursor := $Cursor
+@onready var pointer := $Pointer
+@onready var cursor = $Control/CenterNode/Cursor
 @onready var hint_text_label := $UI/MarginContainer/RichTextLabel
 
 @onready var joypad_up := $UI/Control/JoypadUp
@@ -20,8 +21,7 @@ var nodes: Array[Area2D]
 var TmrNode := preload("res://tmr_node.tscn")
 var grabbing := false
 var grabbing_tmr
-var add_node := false
-var node_to_add := {}
+var adding_node: Area2D
 var selected_shapes := ""
 
 
@@ -33,18 +33,13 @@ var selected_shapes := ""
 ]
 	
 func _ready():
-	#sub_viewport.add_child(game_2)
-	#compicactus_anim.play("Action-07-Swinging")
 	var hint_text := ""
 	var pos_y := 50
 	for lexeme in Global.lexemes:
-		#hint_text += "%s %s\n" % [lexeme.name, lexeme.code]
 		var tmr_node: Area2D = TmrNode.instantiate()
 		tmr_node.position = Global.cursor2d_pos
-		tmr_node.label = lexeme.label
 		tmr_node.code = lexeme.code
 		tmr_node.position = Vector2(200, pos_y)
-		tmr_node.connections_amount = 0
 		lexeme_list.add_child(tmr_node)
 		pos_y += 400
 	hint_text += "\n\n"
@@ -54,13 +49,6 @@ func _ready():
 	hint_text += "t: %s\n" % InputMap.action_get_events("TriangleShape")[0].as_text()
 	hint_text_label.text = hint_text
 	
-#	for lexeme in Global.lexemes:
-#		var button = Button.new()
-#		button.text = lexeme.name
-#		button.focus_mode = Control.FOCUS_NONE
-#		button.connect("button_up", _on_lexeme_button_up.bind(lexeme))
-#		buttons.add_child(button)
-	#print(InputMap.action_get_events("Up")[0].as_text())
 
 func getShapeButton(action_name:String):
 	var info := InputMap.action_get_events(action_name)[0].as_text()
@@ -78,12 +66,6 @@ func _input(event):
 	if event.is_action_pressed("Place"):
 		for node in nodes:
 			print(node.child_tmrs)
-#		if grabbing:
-#			grabbing = false
-#			grabbing_tmr.grabbed = false
-#		else:
-#			add_node = true
-#			node_to_add = Global.lexemes.pick_random()
 	
 	joypad_buttons[getShapeButton("SquareShape")].text = "S"
 	joypad_buttons[getShapeButton("CircleShape")].text = "C"
@@ -101,75 +83,61 @@ func _input(event):
 	elif event.is_action_pressed("TriangleShape"):
 		selected_shapes += "t"
 		joypad_buttons[getShapeButton("TriangleShape")].text = "(T)"
-	elif event.is_action_pressed("LexemeDown"):
+	
+	elif event.is_action_pressed("CursorDown"):
+		doneBuildingNode()
 		var tween = create_tween()
-		tween.tween_property(lexeme_list, "position:y", lexeme_list.position.y + 200, 0.2)
-		#lexeme_list.position.y += 200
-	elif event.is_action_pressed("LexemeUp"):
+		tween.tween_property(cursor, "position:y", cursor.position.y + 300, 0.1)
+	elif event.is_action_pressed("CursorUp"):
+		#cursor.position.y -= 100
+		doneBuildingNode()
 		var tween = create_tween()
-		tween.tween_property(lexeme_list, "position:y", lexeme_list.position.y - 200, 0.2)
-		#lexeme_list.position.y -= 200
+		tween.tween_property(cursor, "position:y", cursor.position.y - 300, 0.1)
+	elif event.is_action_pressed("CursorLeft"):
+		#cursor.position.x -= 100
+		doneBuildingNode()
+		var tween = create_tween()
+		tween.tween_property(cursor, "position:x", cursor.position.x - 300, 0.1)
+	elif event.is_action_pressed("CursorRight"):
+		#cursor.position.x += 100
+		doneBuildingNode()
+		var tween = create_tween()
+		tween.tween_property(cursor, "position:x", cursor.position.x + 300, 0.1)
+
+func doneBuildingNode():
+	selected_shapes = ""
+	adding_node = null
 
 func _process(_delta):
 	var panvector := Vector2.ZERO
 	panvector.x = Input.get_action_strength("PanRight") - Input.get_action_strength("PanLeft")
 	panvector.y = Input.get_action_strength("PanDown") - Input.get_action_strength("PanUp")
-	center_node.position += panvector * _delta * 800
+	center_node.position -= panvector * _delta * 1000
 	
 	var joyvector := Vector2.ZERO
-	joyvector.x = Input.get_action_strength("CursorRight") - Input.get_action_strength("CursorLeft")
-	joyvector.y = Input.get_action_strength("CursorDown") - Input.get_action_strength("CursorUp")
+	joyvector.x = Input.get_action_strength("PointerRight") - Input.get_action_strength("PointerLeft")
+	joyvector.y = Input.get_action_strength("PointerDown") - Input.get_action_strength("PointerUp")
 	#label_score.text = "%f" % Input.get_action_strength("MoveCursorRight")
 	#joyvector = joyvector.normalized()
-	cursor.position += joyvector * _delta * 600
+	#pointer.position += joyvector * _delta * 600
+	lexeme_list.position += joyvector * _delta * 1500
 	if joyvector.length() > 0:
-		if selected_shapes.length() > 0:
-			print(selected_shapes)
-			if codeExists(selected_shapes):
-				add_node = true
-				node_to_add = codeToLexeme(selected_shapes)
 		selected_shapes = ""
-	#cursor3D.position = Global.cursor3d_pos
-	if add_node:
-		add_node = false
-		var tmr_node: Area2D = TmrNode.instantiate()
-		tmr_node.position = game_node.to_local(Global.cursor2d_pos)
-		tmr_node.connect("input_event", _on_input_event.bind(tmr_node))
-		var lexeme = node_to_add
-		tmr_node.label = lexeme.label
-		tmr_node.code = lexeme.code
-		tmr_node.connections_amount = lexeme.connections_amount
-		game_node.add_child(tmr_node)
-		#tmr_node.grabbed = true
-		#grabbing_tmr = tmr_node
-		#grabbing = true
-		nodes.append(tmr_node)
-		print("Add child")
+		adding_node = null
+	if selected_shapes.length() == 1:
+		if adding_node == null:
+			print(selected_shapes)
+			var tmr_node: Area2D = TmrNode.instantiate()
+			tmr_node.position = game_node.to_local(center_node.to_global(Global.cursor2d_pos))
+			tmr_node.connect("input_event", _on_input_event.bind(tmr_node))
+			tmr_node.code = selected_shapes
+			game_node.add_child(tmr_node)
+			nodes.append(tmr_node)
+			adding_node = tmr_node
+			print("Add child")
+	elif selected_shapes.length() > 1:
+		adding_node.code = selected_shapes
 	Global.cursor2d_pos = cursor.position
-	
-#	var xs: float = float(get_viewport().size.x) / 960
-#	var ys: float = float(get_viewport().size.y) / 540
-#	if xs > ys:
-#		print("x")
-#		center_node.position.x = get_viewport().size.x
-#	else:
-#		print("y")
-#		center_node.position.y = get_viewport().size.y
-#	#print(center_node.position, DisplayServer.window_get_size(0))
-#	#print(get_viewport().size)
-#	#center_node.position = get_viewport().size / 2
-
-func codeExists(code:String):
-	for lexeme in Global.lexemes:
-		if lexeme.code == code:
-			return true
-	return false
-
-func codeToLexeme(code:String) -> Dictionary:
-	for lexeme in Global.lexemes:
-		if lexeme.code == code:
-			return lexeme
-	return {}
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
@@ -195,9 +163,10 @@ func _on_input_event(_viewport:Node, event:InputEvent, _shape_idx:int, tmr_node:
 				grabbing = true
 
 func _on_lexeme_button_up(lexeme):
-	add_node = true
-	node_to_add = lexeme
+	pass
+#	add_node = true
+#	node_to_add = lexeme
 
 func getCursorPosition(event):
-	cursor.position = event.position
+	pointer.position = event.position
 
--- tmr_node.gd
+++ tmr_node.gd
@@ -4,9 +4,12 @@ extends Area2D
 @export var code := "------"
 @export_range(0, 5) var connections_amount := 3
 
+var last_code := "------"
+
 @onready var lasers_node := $LasersNode
 @onready var rays_node := $RaysNode
 @onready var label2d := %TMRLabel
+@onready var shapes_node = $Shapes
 #@onready var code_label = %ShapesLabel
 
 @onready var shape_sprites: Array[Sprite2D] = [
@@ -17,13 +20,19 @@ extends Area2D
 	$Shapes/Shape05
 ]
 
+@onready var node_sprites: Array[Sprite2D] = [
+	$Node1,
+	$Node3,
+	$Node2
+]
+
 var mat_laser_idle = preload("res://materials/laser_idle.tres")
 var mat_laser_active = preload("res://materials/laser_active.tres")
 
-var circle_texture = preload("res://atlas/circle.tres")
-var cross_texture = preload("res://atlas/cross.tres")
-var square_texture = preload("res://atlas/square.tres")
-var triangle_texture = preload("res://atlas/triangle.tres")
+var circle_texture = preload("res://atlas/circle_small.tres")
+var cross_texture = preload("res://atlas/cross_small.tres")
+var square_texture = preload("res://atlas/square_small.tres")
+var triangle_texture = preload("res://atlas/triangle_small.tres")
 
 var grabbed := false
 var mouse_position := Vector2(0,0)
@@ -43,6 +52,47 @@ var laser_positions = [
 ]
 
 func _ready():
+	pass
+
+func _physics_process(_delta):
+	if grabbed:
+		global_position = Global.cursor2d_pos
+
+func _process(_delta):
+	update()
+
+func checkRays():
+	for ray in connections_amount:
+		var start_point = rays[ray].position
+		if rays[ray].is_colliding():
+			var global_collision_point = rays[ray].get_collision_point()
+			var collision_tmr := rays[ray].get_collider()
+			child_tmrs[ray] = collision_tmr
+			lasers[ray].set_point_position(1, lasers[ray].to_local(global_collision_point))
+			lasers[ray].default_color = Color(0.961, 0.247, 0.588)
+			lasers[ray].self_modulate.a = 0.8
+		else:
+			var global_target_pos = rays[ray].to_global(rays[ray].target_position)
+			lasers[ray].set_point_position(1, lasers[ray].to_local(global_target_pos))
+			lasers[ray].default_color = Color(0.725, 0.537, 0.925)
+			lasers[ray].self_modulate.a = 0.2
+
+func update():
+	updateCode()
+	for n in node_sprites:
+		n.visible = false
+#	for n in connections_amount:
+#		node_sprites[n].visible = true
+
+func updateOld():
+	updateCode()
+	lasers.resize(0)
+	rays.resize(0)
+	for c in lasers_node.get_children():
+		c.free()
+	for c in rays_node.get_children():
+		c.free()
+	return
 	for n in connections_amount:
 		var laser := Line2D.new()
 		laser.width = 24
@@ -60,16 +110,27 @@ func _ready():
 		rays_node.add_child(ray)
 		child_tmrs.append(null)
 
-func _physics_process(_delta):
-	if grabbed:
-		global_position = Global.cursor2d_pos
-
-func _process(_delta):
-	label2d.text = label
+func updateCode():
+	if !codeExists(code):
+		return
+	if last_code == code:
+		return
+	last_code = code
+	var lexeme = codeToLexeme(code)
+	connections_amount = lexeme.connections_amount
+	shapes_node.position.x = 80 - (lexeme.connections_amount * 13)
+	label2d.text = lexeme.label
 	#code_label.text = code
 	for s in shape_sprites:
 		s.texture = null
 	for n in code.length():
+		shape_sprites[n].scale = Vector2(0, 0)
+		var tween = create_tween()
+		tween.set_loops()
+		tween.tween_property(shape_sprites[n], "scale", Vector2(0, 0), n*0.5)
+		tween.tween_property(shape_sprites[n], "scale", Vector2(1, 1), 0.5+(n*0.5))
+		tween.tween_property(shape_sprites[n], "scale", Vector2(1, 1), 5)
+		tween.tween_property(shape_sprites[n], "scale", Vector2(0, 0), 1)
 		match code[n]:
 			"s":
 				shape_sprites[n].texture = square_texture
@@ -79,20 +140,7 @@ func _process(_delta):
 				shape_sprites[n].texture = triangle_texture
 			"x":
 				shape_sprites[n].texture = cross_texture
-	for ray in connections_amount:
-		var start_point = rays[ray].position
-		if rays[ray].is_colliding():
-			var global_collision_point = rays[ray].get_collision_point()
-			var collision_tmr := rays[ray].get_collider()
-			child_tmrs[ray] = collision_tmr
-			lasers[ray].set_point_position(1, lasers[ray].to_local(global_collision_point))
-			lasers[ray].default_color = Color(0.961, 0.247, 0.588)
-			lasers[ray].self_modulate.a = 0.8
-		else:
-			var global_target_pos = rays[ray].to_global(rays[ray].target_position)
-			lasers[ray].set_point_position(1, lasers[ray].to_local(global_target_pos))
-			lasers[ray].default_color = Color(0.725, 0.537, 0.925)
-			lasers[ray].self_modulate.a = 0.2
+	
 
 #func _on_input_event(_viewport, _event, _shape_idx):
 #	pass
@@ -112,3 +160,15 @@ func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		event = event as InputEventMouseMotion
 		mouse_position = event.position
+
+func codeExists(code:String):
+	for lexeme in Global.lexemes:
+		if lexeme.code == code:
+			return true
+	return false
+
+func codeToLexeme(code:String) -> Dictionary:
+	for lexeme in Global.lexemes:
+		if lexeme.code == code:
+			return lexeme
+	return {}
